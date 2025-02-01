291a5099fe3f6b7359755e18471c2914f4f351b5
gnostic <64601257+nynxa@users.noreply.github.com>
2025-02-01 14:39:04 -0500

    feat: add env.d.ts file handling in type generation script
    - Introduced a check for the existence of the env.d.ts file before generating types.
    - Added user prompt to confirm overwriting the existing env.d.ts file.
    - Retained functionality to gather .env files while excluding .env.example.
    - Improved user experience by providing feedback during the process.

--- src/bin.ts
+++ src/bin.ts
@@ -2,11 +2,17 @@ import { generateEnvTypes } from './plugin'
 import { getEnvFiles } from './utils'
 
 console.log('generating types from all .env files in the project...')
-const envFiles = await getEnvFiles(new Bun.Glob('.env*'), ['.env.example'])
-if (envFiles.length === 0) {
-	console.log('no .env files found')
-	process.exit(1)
+const existingDtsFile = Bun.file('env.d.ts')
+if (await existingDtsFile.exists()) {
+	console.log('env.d.ts file already exists, overwrite? (y/n)')
+	const overwrite =
+		await Bun.$`read -p "Enter your choice: " overwrite && echo $overwrite`.text()
+	if (overwrite !== 'y') {
+		console.log('exiting...')
+		process.exit(1)
+	}
 }
+const envFiles = await getEnvFiles(new Bun.Glob('.env*'), ['.env.example'])
 
 await generateEnvTypes({ envFiles })
 console.log('types generated successfully!')
